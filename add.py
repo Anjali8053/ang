cffe12d953a5c01cdd7a999325ac4526afa2820a
Anjali8053 <anjali805342@gmail.com>
2025-06-02 12:36:25 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,19 @@
+x=500
+y=500
+z=x+y
+print(z)
+
+
+input=[ 1,2,3,4]
+output=3
+print("output is",input[3])
+
+number=[6,7,8,10]
+into=10
+print("number is",number[3])
+
+
+fruit=[ "orange","Apple","Grapes"]
+print("ans is",fruit)
+
+
